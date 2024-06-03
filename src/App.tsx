72fdbfe72d2600f18cefdb6f345538399cb35ca1
Vitor Moutim <moutimg@gmail.com>
2024-06-03 20:54:46 -0300

    feat: create and style navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './modules/Home/Home';
+import Navbar from './modules/Navbar/Navbar';
+import './App.css';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-      </Routes>
+      <Navbar />
+      <main className='limiter'>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+        </Routes>
+      </main>
     </Router>
   );
 };
